0b05ed610c6e813f5e0d561e292532414103d771
christopher <kempt09@gmail.com>
2016-02-18 15:45:23 -0500

    add navbar

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 $(function(){
 	
 	$.get('https://www.reddit.com/r/aww/.json', function(content){
+
 		var count = 0;
 		var innerCount = 0;
 		var titlesPerRow = 3;
 		var numberOfArticle = 12;
 		var bootSize = (6/titlesPerRow).toString();
+
+		(function(){
 		
-	
+			for(var i = 0; i<numberOfArticle ; i++){
 
-		for(var i = 0; i<numberOfArticle ; i++){
-			if (count % titlesPerRow ===0 || count === 0) {
-				innerCount += 1;
-			};
-
+				if (count % titlesPerRow ===0 || count === 0) {
+					innerCount += 1;
+				};
 
 			var title = content.data.children[i].data.title;
 			var thumbnail = content.data.children[i].data.thumbnail;
@@ -22,19 +23,24 @@ $(function(){
 			var score = content.data.children[i].data.score;
 			var comments = content.data.children[i].data.score;
 
+			(function(){
+
+				var tally = $('<p> Author: ' + author + '</p>' + '<p> Score: ' + score + '</p>' + '<p> Comments: ' + comments + '</p>' )
+				var articleTitle = $("<a class='damageControl' href='"+ url +"'><h3 class='articleTitle'></h3></a>").text(title);
+				var articleImage = $("<img class='damageControl' />").attr('src', thumbnail);
+
+				var divCreation1 = $('<div class="col-xs-'+ bootSize+ '"></div>').html(articleImage);
+				var divCreation2 = $('<div class="col-xs-'+ bootSize+ '"></div>').html([articleTitle, tally]);
 
 
-			var articleTitle = $("<a class='damageControl' href='"+ url +"'><h3 class='articleTitle'></h3></a>").text(title);
-			var articleImage = $("<img class='damageControl' />").attr('src', thumbnail);
+				$('#row'+innerCount).append([divCreation1, divCreation2]);
+				count += 1;
+				console.log(articleTitle);
 
-			var divCreation1 = $('<div class="col-xs-'+ bootSize+ '"></div>').html(articleImage);
-			var divCreation2 = $('<div class="col-xs-'+ bootSize+ '"></div>').html(articleTitle);
+			})();
 
+			}
 
-			$('#row'+innerCount).append([divCreation1, divCreation2]);
-			count += 1;
-			console.log(articleTitle);
-
-		}
+		})();
 	})
 });
