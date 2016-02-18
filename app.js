06ee808bee89bc3d75448a1b90ba8612c9be8184
christopher <kempt09@gmail.com>
2016-02-18 17:45:12 -0500

    changed colors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ $(function(){
 			var innerCount = 0;
 			var titlesPerRow = 3;
 			var numberOfArticle = 12;
-			var bootSize = (6/titlesPerRow).toString();
+			// var bootSize = (6/titlesPerRow).toString();
 
 			(function(){
 			
@@ -25,19 +25,19 @@ $(function(){
 
 				(function(){
 
-				var tally = $('<p> <strong>Author</strong>: ' + author + '</p>' + '<p> <strong>Score</strong>: ' + score + '</p>' + '<p> <strong>Comments</strong>: ' + comments + '</p>' )
-				var articleTitle = $("<a class='damageControl' href='"+ url +"'><h3 class='articleTitle'></h3></a>").text(title);
-				var articleImage = $("<img class='damageControl' />").attr('src', thumbnail);
+					var tally = $('<p> <strong>Author</strong>: ' + author + '</p>' + '<p> <strong>Score</strong>: ' + score + '</p>' + '<p> <strong>Comments</strong>: ' + comments + '</p>' )
+					var articleTitle = $("<a class='damageControl' href='"+ url +"'><h3 class='articleTitle'></h3></a>").text(title);
+					var articleImage = $("<img class='damageControl' />").attr('src', thumbnail);
 
-				 var divCreation1 = $('<div class="col-xs-'+ bootSize+ ' artBlock"></div>').html([articleTitle,articleImage, tally]);
-				// // var divCreation2 = $('<div class="col-xs-'+ bootSize+ '"></div>').html([articleTitle, tally]);
+					//  var divCreation1 = $('<div class="col-xs-'+ bootSize + '"></div>').html([articleTitle,articleImage, tally]);
+					// // // var divCreation2 = $('<div class="col-xs-'+ bootSize+ '"></div>').html([articleTitle, tally]);
 
 
-					// var divCreation1 = $('<div class="col-xs-12 col-md-2"></div>').html(articleImage);
-					// var divCreation2 = $('<div class="col-xs-12 col-md-2"></div>').html([articleTitle, tally]);
+					var divCreation1 = $('<div class="col-xs-12 col-md-2"></div>').html(articleImage);
+					var divCreation2 = $('<div class="col-xs-12 col-md-2"></div>').html([articleTitle, tally]);
 
-				$('#row'+innerCount).append(divCreation1);
-				count += 1;
+					$('#row'+innerCount).append([divCreation1, divCreation2]);
+					count += 1;
 
 				})();
 
