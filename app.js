800d327ff8e9d3eb05d4ec5ee1dd27fbea5fedf5
christopher <kempt09@gmail.com>
2016-02-18 15:11:33 -0500

    fixed conflicts

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 $(function(){
+	
 	$.get('https://www.reddit.com/r/aww/.json', function(content){
 		var count = 0;
 		var innerCount = 0;
@@ -6,16 +7,10 @@ $(function(){
 		var numberOfArticle = 12;
 		var bootSize = (6/titlesPerRow).toString();
 		
-		
-<<<<<<< HEAD
-		for(var i = 0; i<12 ; i++){
-
-			if (count % 3 ===0 || count === 0) {
-=======
+	
 
 		for(var i = 0; i<numberOfArticle ; i++){
 			if (count % titlesPerRow ===0 || count === 0) {
->>>>>>> a2d5dadfacae9d9e91d06dd0d6cdc8a84837b4d6
 				innerCount += 1;
 			};
 
@@ -31,14 +26,9 @@ $(function(){
 
 			var articleTitle = $("<a class='damageControl' href='"+ url +"'><h3 class='articleTitle'></h3></a>").text(title);
 			var articleImage = $("<img class='damageControl' />").attr('src', thumbnail);
-<<<<<<< HEAD
-			var divCreation1 = $('<div class="col-xs-2"></div>').html(articleImage);
-			var divCreation2 = $('<div class="col-xs-2"></div>').html(articleTitle);
-=======
+
 			var divCreation1 = $('<div class="col-xs-'+ bootSize+ '"></div>').html(articleImage);
 			var divCreation2 = $('<div class="col-xs-'+ bootSize+ '"></div>').html(articleTitle);
->>>>>>> a2d5dadfacae9d9e91d06dd0d6cdc8a84837b4d6
-			
 
 
 			$('#row'+innerCount).append([divCreation1, divCreation2]);
