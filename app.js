f7f84e8bcd3a05e34e278185f6bb51bc1a9c6822
christopher <kempt09@gmail.com>
2016-02-18 15:08:55 -0500

    changes

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ $(function(){
 		
 		
 		for(var i = 0; i<12 ; i++){
+
 			if (count % 3 ===0 || count === 0) {
 				innerCount += 1;
 			};
+
+
 			var title = content.data.children[i].data.title;
 			var thumbnail = content.data.children[i].data.thumbnail;
 			var url = content.data.children[i].data.url;
@@ -21,8 +24,8 @@ $(function(){
 
 			var articleTitle = $("<a class='damageControl' href='"+ url +"'><h3 class='articleTitle'></h3></a>").text(title);
 			var articleImage = $("<img class='damageControl' />").attr('src', thumbnail);
-			var divCreation1 = $('<div class="col-xs-2></div>').html(articleImage);
-			var divCreation2 = $('<div class="col-xs-2></div>').html(articleTitle);
+			var divCreation1 = $('<div class="col-xs-2"></div>').html(articleImage);
+			var divCreation2 = $('<div class="col-xs-2"></div>').html(articleTitle);
 			
 
 
