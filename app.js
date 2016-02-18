0da95a236874fcbc1032ed608b4e739b788a3746
christopher <kempt09@gmail.com>
2016-02-18 16:03:58 -0500

    fixed stuff

--- app.js
+++ app.js
@@ -1,47 +1,65 @@
-$(function(){
-	
-	$.get('https://www.reddit.com/r/aww/.json', function(content){
+$(function(){  	
 
-		var count = 0;
-		var innerCount = 0;
-		var titlesPerRow = 3;
-		var numberOfArticle = 12;
-		var bootSize = (6/titlesPerRow).toString();
-
-		(function(){
+	function(){
 		
-			for(var i = 0; i<numberOfArticle ; i++){
+		$.get('https://www.reddit.com/r/aww/.json', function(content){
 
-				if (count % titlesPerRow ===0 || count === 0) {
-					innerCount += 1;
-				};
-
-			var title = content.data.children[i].data.title;
-			var thumbnail = content.data.children[i].data.thumbnail;
-			var url = content.data.children[i].data.url;
-			var author = content.data.children[i].data.author;
-			var score = content.data.children[i].data.score;
-			var comments = content.data.children[i].data.score;
+			var count = 0;
+			var innerCount = 0;
+			var titlesPerRow = 3;
+			var numberOfArticle = 12;
+			var bootSize = (6/titlesPerRow).toString();
 
 			(function(){
+			
+				for(var i = 0; i<numberOfArticle ; i++){
 
-				var tally = $('<p> Author: ' + author + '</p>' + '<p> Score: ' + score + '</p>' + '<p> Comments: ' + comments + '</p>' )
-				var articleTitle = $("<a class='damageControl' href='"+ url +"'><h3 class='articleTitle'></h3></a>").text(title);
-				var articleImage = $("<img class='damageControl' />").attr('src', thumbnail);
+					if (count % titlesPerRow ===0 || count === 0) {
+						innerCount += 1;
+					};
 
-				var divCreation1 = $('<div class="col-xs-'+ bootSize+ '"></div>').html(articleImage);
-				var divCreation2 = $('<div class="col-xs-'+ bootSize+ '"></div>').html([articleTitle, tally]);
+				var title = content.data.children[i].data.title;
+				var thumbnail = content.data.children[i].data.thumbnail;
+				var url = content.data.children[i].data.url;
+				var author = content.data.children[i].data.author;
+				var score = content.data.children[i].data.score;
+				var comments = content.data.children[i].data.score;
+
+				(function(){
+
+					var tally = $('<p> Author: ' + author + '</p>' + '<p> Score: ' + score + '</p>' + '<p> Comments: ' + comments + '</p>' )
+					var articleTitle = $("<a class='damageControl' href='"+ url +"'><h3 class='articleTitle'></h3></a>").text(title);
+					var articleImage = $("<img class='damageControl' />").attr('src', thumbnail);
+
+					var divCreation1 = $('<div class="col-xs-'+ bootSize+ '"></div>').html(articleImage);
+					var divCreation2 = $('<div class="col-xs-'+ bootSize+ '"></div>').html([articleTitle, tally]);
 
 
-				$('#row'+innerCount).append([divCreation1, divCreation2]);
-				count += 1;
-				console.log(articleTitle);
+					$('#row'+innerCount).append([divCreation1, divCreation2]);
+					count += 1;
+					console.log(articleTitle);
 
+
+				})();
+
+				}
 
 			})();
+		});
+	}
 
-			}
-
-		})();
-	})
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
