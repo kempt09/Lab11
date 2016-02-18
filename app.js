73dc0f58b4ea4b1687fe8da671768ec1a418ace1
Edwin A <ea976@nyu.edu>
2016-02-18 15:05:47 -0500

    Easily Adjustable Row Sizes

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ $(function(){
 	$.get('https://www.reddit.com/r/aww/.json', function(content){
 		var count = 0;
 		var innerCount = 0;
-		//var titlesPerRow = 3;
-		//var bootSize = (6/titlesPerRow).toString();
+		var titlesPerRow = 2;
+		var bootSize = (6/titlesPerRow).toString();
 		
 		
 		for(var i = 0; i<10 ; i++){
-			if (count % 3 ===0 || count === 0) {
+			if (count % titlesPerRow ===0 || count === 0) {
 				innerCount += 1;
 			};
 			var title = content.data.children[i].data.title;
@@ -21,8 +21,8 @@ $(function(){
 
 			var articleTitle = $("<a class='damageControl' href='"+ url +"'><h3 class='articleTitle'></h3></a>").text(title);
 			var articleImage = $("<img class='damageControl' />").attr('src', thumbnail);
-			var divCreation1 = $('<div class="col-xs-2></div>').html(articleImage);
-			var divCreation2 = $('<div class="col-xs-2></div>').html(articleTitle);
+			var divCreation1 = $('<div class="col-xs-'+ bootSize+ '"></div>').html(articleImage);
+			var divCreation2 = $('<div class="col-xs-'+ bootSize+ '"></div>').html(articleTitle);
 			
 
 
