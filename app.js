477f67955791f4fac63ad3c05fbfe49b70db65e0
Edwin A <ea976@nyu.edu>
2016-02-18 14:41:54 -0500

    Rows of three view

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 $(function(){
 	$.get('https://www.reddit.com/r/aww/.json', function(content){
-		for(var i = 0;i<10;i++){
+		var count = 0;
+		var innerCount = 0;
+		
+		
+		for(var i = 0; i<10 ; i++){
+			if (count % 3 ===0 || count === 0) {
+				innerCount += 1;
+			}
 			var title = content.data.children[i].data.title;
 			var thumbnail = content.data.children[i].data.thumbnail;
 			var url = content.data.children[i].data.url;
 
 
 			var articleTitle = $("<a class='damageControl' href='"+ url+"'><h3 class='articleTitle'></h3></a>").text(title);
-			// var linkWord = $("<a></a>").attr('href', url).html(articleTitle);
 			var articleImage = $("<img class='damageControl' />").attr('src', thumbnail);
-			var divCreation = $('<div class="col-xs-12"></div>').html([articleTitle, articleImage]);
+			var divCreation1 = $('<div class="col-xs-2"></div>').html(articleImage);
+			var divCreation2 = $('<div class="col-xs-2"></div>').html(articleTitle);
 			
-			$('#articles').append(divCreation);
+
+
+			$('#row'+innerCount).append([divCreation1, divCreation2]);
+			count += 1;
+			console.log(count);
+			//console.log(innerCount);
 
 		}
 	})
